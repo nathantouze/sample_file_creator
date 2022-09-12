00f9a1ac8c733676a16e2089a3ce968ee2eebce9
Nathan Touzé <60022007+nathantouze@users.noreply.github.com>
2022-09-12 12:39:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import argparse
 from io import TextIOWrapper
+import sys
 
 
 KB = 1024
 MB = KB * 1024
 GB = MB * 1024
 
-def write_in_file(file: TextIOWrapper, value: int, multiplier: int):
-    file.write("." * (value * multiplier))
+
+def write_in_file(file: TextIOWrapper, value: int, filename: str):
+    if value > 200 * MB:
+        end = value / (MB * 100)
+        written = 0
+        progress = 0
+        for i in range(int(end)):
+            file.write("." * (MB * 100))
+            written += MB * 100
+            progress = int((written / value) * 100)
+            sys.stdout.write("\rProgress: %i%%" % (progress))
+            sys.stdout.flush()
+        left_to_write = value - written
+        file.write("." * left_to_write)
+        sys.stdout.write("\rProgress: 100%%\nDone. Your file is here: %s\n" % filename)
+        sys.stdout.flush()
+    else:
+        file.write("." * value)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -16,11 +33,9 @@ if __name__ == "__main__":
     parser.add_argument("-mb", type=int, help="Size in Kilobyte of the content", default=0)
     parser.add_argument("-gb", type=int, help="Size in Gigabyte of the content", default=0)
     parser.add_argument("-o", help="Output filepath", default="output.txt")
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     f = open(args.o, "a")
-    write_in_file(f, args.b, 1)
-    write_in_file(f, args.kb, KB)
-    write_in_file(f, args.mb, MB)
-    write_in_file(f, args.gb, GB)
+    to_write = args.b + args.kb * KB + args.mb * MB + args.gb * GB
+    write_in_file(f, to_write, args.o)
     f.close()
